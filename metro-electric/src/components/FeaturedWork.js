d0e1da040e0ae5dec8c124cb164050adea739789
Beth Howell <beahowel117@gmail.com>
2023-05-02 15:51:48 -0400

    added main pages

---
+++ metro-electric/src/components/FeaturedWork.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+function FeaturedWork() {
+  return (
+    <>
+      <Nav />
+      <div>FeaturedWork</div>
+    </>
+  )
+}
+
+export default FeaturedWork
---
+++ metro-electric/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav'
+
+function Home() {
+  return (
+    <div>
+      <Nav/>
+      <div>Home</div>
+    </div>
+  )
+}
+
+export default Home
