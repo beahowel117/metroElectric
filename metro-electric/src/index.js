c919a62a39358daf23bb0a5410e37b6d92d4801c
Beth Howell <beahowel117@gmail.com>
2023-05-02 11:13:47 -0400

    updated for react 18

--- metro-electric/src/index.js
+++ metro-electric/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+/** material ui */
+//import { MuiThemeProvider, CssBaseline } from '@mater'
+/**react 18 */
+const container = document.getElementById('app');
+const root = createRoot(container);
+// root.render(<App />);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
