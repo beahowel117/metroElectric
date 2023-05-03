e336f03138ade1a612ee7f2a2ac6e13a8070f694
Beth Howell <beahowel117@gmail.com>
2023-05-03 16:10:26 -0400

    added about home image

--- metro-electric/src/App.js
+++ metro-electric/src/App.js
@@ -2,13 +2,14 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import FeaturedWork from "./components/FeaturedWork";
+import About from "./components/About";
 import Box from "@mui/material/Box";
 
 function App() {
   return (
     <Box>
       <Nav />
-      <FeaturedWork />
+      <About />
     </Box>
   );
 }
