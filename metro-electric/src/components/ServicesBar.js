2dc11871e812f7e0c6c02b836f21dfbacaa8ad76
Beth Howell <beahowel117@gmail.com>
2023-05-03 09:51:21 -0400

    added services component

---
+++ metro-electric/src/components/ServicesBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**THis is the grey section with the 5 clickable photos used throughout the platform */
+function ServicesBar() {
+  return (
+    <div>ServicesBar</div>
+  )
+}
+
+export default ServicesBar
