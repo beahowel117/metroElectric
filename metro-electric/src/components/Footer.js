bf345f9cbf043a9223bf92f3e2b7814a857fe629
Beth Howell <beahowel117@gmail.com>
2023-05-04 15:34:41 -0400

    Fix footer

--- metro-electric/src/components/Footer.js
+++ metro-electric/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { Button, Typography, Box } from "@mui/material";
 import MetroTekLogo from "../images/logos/MetroTekLogo.jpg";
 import NECALogo from "../images/logos/NECA-logo.jpg";
@@ -16,24 +16,45 @@ function Footer() {
   };
   return (
    <Box
-    display='flex'
-    justifyContent='center'
     borderTop='5px solid blue'
     width='797px'
     margin='40px auto 0'
    >
     <Box
+      display='flex'
+      justifyContent='center'
       paddingTop='8px'
-
     >
       <NavLink
         to="/"
       >
-      <a href=" " className="footer-logo">
-        <img src={MetroTekLogo} alt="Footer Logo" style={styles.footerLogo}/>
+        <a href=" " className="footer-logo">
+         <img src={MetroTekLogo} alt="Footer Logo" style={styles.footerLogo}/>
         </a>
       </NavLink>
     </Box>
+      <Box
+        display='flex'
+        justifyContent='space-around'
+      >
+        <Link
+          to="/"
+          style={{textDecoration: "none", color: "black"}}
+        >HOME</Link>
+         <Link
+          style={{textDecoration: "none", color: "black"}}
+          to="/services"
+          >SERVICES</Link>
+        <Link
+          to="/work"
+        >FEATURED WORK</Link>
+        <Link
+          to="/about"
+        >ABOUT</Link>
+        <Link
+          to="/contact"
+        >CONTACT</Link>
+      </Box>
    </Box>
   )
 }
