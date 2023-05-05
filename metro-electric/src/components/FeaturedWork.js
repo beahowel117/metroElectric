3ead3b5f89a2f4a53aa4be6d16d45d052e0911f5
Beth Howell <beahowel117@gmail.com>
2023-05-05 16:44:52 -0400

    added featuredwork images

--- metro-electric/src/components/FeaturedWork.js
+++ metro-electric/src/components/FeaturedWork.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import featuredWork from "../images/FeaturedWork/featuredWork(2000X1000).jpg";
-import "../index.css";
 import Footer from "./Footer";
+
+import featuredWork from "../images/FeaturedWork/featuredWork(2000X1000).jpg";
+import overHeadConstruction from "../images/FeaturedWork/FW-OC.JPG";
+import controlledAgriculture from "../images/FeaturedWork/FW-CAE.JPG";
+import transformerReplacement from "../images/FeaturedWork/FW-TR.jpg";
+import renewableEnergy from "../images/FeaturedWork/FW-RE.jpg";
+import stormRestoration from "../images/FeaturedWork/FW-SR.jpg";
+
+
+import "../index.css";
+
 
 function FeaturedWork() {
   const styles = {
@@ -14,6 +23,30 @@ function FeaturedWork() {
       backgroundSize: "cover",
     },
   };
+
+  const imageGallery = [
+    {
+      img: overHeadConstruction,
+      text: "Overhead Construction"
+    },
+    {
+      img: controlledAgriculture,
+      text: "Controlled Environment Agriculture"
+    },
+    {
+      img: transformerReplacement,
+      text: "Transformer Replacements"
+    },
+    {
+      img: renewableEnergy,
+      text: "Renewable Energy"
+    },
+    {
+      img: stormRestoration,
+      text: "Storm Restoration"
+    }
+  ];
+  
   return (
     <>
       <Box
