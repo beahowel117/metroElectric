af4d7230715be13f5836938bb4147232176db778
Eugene Sokoletsky <eugene@eugenes-mbp.mynetworksettings.com>
2023-05-05 14:05:02 -0400

    playing with carousel

--- metro-electric/src/components/Home.js
+++ metro-electric/src/components/Home.js
@@ -7,35 +7,72 @@ import SOLUTIONS from "../images/HomeCarousel/solutions-4 (2x1).jpg";
 import COMMUNITY from "../images/HomeCarousel/community-5(2x1).jpg";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "../carousel.css";
+import "../index.css";
 import GreyGallery from "./GreyGallery";
 import Footer from "./Footer";
 
 function Home() {
+  const carouselContent = [
+    { img: HOME },
+    { img: EIS },
+    { img: POWER },
+    { img: SOLUTIONS },
+    { img: COMMUNITY },
+  ];
+
+  const fadeAnimationHandler = (state) => {
+    const transitionTime = "1s";
+    const transitionTimingFunction = "move ease forward";
+
+    let slideStyle = {
+      width: "100%",
+      animation: "move 5s ease ",
+
+      // transitionTimingFunction: transitionTimingFunction,
+      // msTransitionTimingFunction: transitionTimingFunction,
+      // MozTransitionTimingFunction: transitionTimingFunction,
+      // WebkitTransitionTimingFunction: transitionTimingFunction,
+      // OTransitionTimingFunction: transitionTimingFunction,
+    };
+
+    if (!state.swiping) {
+      slideStyle = {
+        ...slideStyle,
+        WebkitTransitionDuration: transitionTime,
+        MozTransitionDuration: transitionTime,
+        OTransitionDuration: transitionTime,
+        transitionDuration: transitionTime,
+        msTransitionDuration: transitionTime,
+      };
+
+      return {
+        slideStyle,
+        selectedStyle: { ...slideStyle, opacity: 1, position: "relative" },
+        prevStyle: { ...slideStyle },
+      };
+    }
+  };
+
   return (
     <Box>
-      <Carousel showThumbs={false} infiniteLoop autoPlay>
-        <div className='image'>
-          <img src={HOME} alt='' />
-          <p className='home'>OVER 45 YEARS</p>
-        </div>
-        <div className='image'>
-          <img src={EIS} alt='' />
-          <p className='eis'>Legend 1</p>
-        </div>
-        <div className='image'>
-          <img src={POWER} alt='' />
-          <p className='power'>Legend 3</p>
-        </div>
-        <div className='image'>
-          <img src={SOLUTIONS} alt='' />
-          <p className='legend'>Legend 3</p>
-        </div>
-        <div className='image'>
-          <img src={COMMUNITY} alt='' />
-          <p className='legend'>Legend 3</p>
-        </div>
+      <Carousel
+        animationHandler={fadeAnimationHandler}
+        showThumbs={false}
+        infiniteLoop
+        autoPlay
+        display='flex'
+        justify
+        content='center'
+        alignItems='center'
+      >
+        {carouselContent.map(({ img }) => (
+          <div className='image-wrap'>
+            <img src={img} alt='' width='120%' />
+            <p className='home'>OVER 45 YEARS</p>
+          </div>
+        ))}
       </Carousel>
+
       <GreyGallery />
       <Footer />
     </Box>
