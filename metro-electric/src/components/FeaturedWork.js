a9753d1bf15ce737401d617a3f2835a4f1b9a26e
Beth Howell <beahowel117@gmail.com>
2023-05-18 17:02:05 -0400

    FIX featured work

--- metro-electric/src/components/FeaturedWork.js
+++ metro-electric/src/components/FeaturedWork.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { NavLink, Link as RouterLink } from "react-router-dom";
 
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, ButtonBase } from "@mui/material";
 import { Card, CardOverflow, AspectRatio, CardCover } from "@mui/joy";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import featuredWork from "../images/FeaturedWork/featuredWork(2000X1000).jpg";
 import overHeadConstruction from "../images/FeaturedWork/FW-OC.JPG";
@@ -23,7 +24,7 @@ function FeaturedWork() {
       backgroundSize: "cover",
     },
   };
-
+  const lessThan900 = useMediaQuery("(max-width:900px)");
   const imageGallery = [
     {
       img: overHeadConstruction,
@@ -51,7 +52,7 @@ function FeaturedWork() {
       link: "/portfolio-items/storm",
     },
   ];
-  
+
   return (
     <>
       <Box className='image-container'>
@@ -72,7 +73,47 @@ function FeaturedWork() {
           gap='10px'
           py='40px'
         >
-          {imageGallery.map(({ img, text, link }) => (
+          {imageGallery.map(({ img, text, link }) => {
+            const isCEA = text === "Controlled Environment Agriculture";
+            return (
+            lessThan900 ?
+             <Card
+                variant='outlined'
+                className='container'
+                sx={{ minWidth: { xs: "85%", md: "" } }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio='5/4'>
+                    <Box
+                      position="relative"
+                    >
+                      <img src={img} loading='lazy' alt='' width='100%' />
+                      {/* <div className='overlayMobile'> */}
+                        <ButtonBase
+                          sx={{
+                            color:"white",
+                            fontSize:{xs: "30px", md: "50px"},
+                            fontWeight: "900",
+                            position:'absolute',
+                            left: "20%",
+                            ...(text === "Controlled Environment Agriculture" && {
+                              right:'18%',
+                            }),
+                          }
+                        }
+                          component={NavLink}
+                          to={link}
+                        >
+                          {text}
+                        </ButtonBase>
+                      </Box>
+                    {/* </div> */}
+                  </AspectRatio>
+                </CardOverflow>
+              </Card>
+
+             :
+
             <Card
               variant='outlined'
               className='container'
@@ -84,6 +125,8 @@ function FeaturedWork() {
                   <div className='overlay'>
                     <Typography
                       className='imgText'
+                      // fontSize={{xs: "17px",sm:"30px", md: "20px"}}
+                      fontWeight="900"
                       component={NavLink}
                       to={link}
                       sx={{ textDecoration: "none" }}
@@ -94,7 +137,8 @@ function FeaturedWork() {
                 </AspectRatio>
               </CardOverflow>
             </Card>
-          ))}
+            )
+            })}
         </Box>
       </Box>
     </>
