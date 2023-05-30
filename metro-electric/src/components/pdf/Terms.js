84ff577b4fef452df5e64eff2edc7bbf31f5fe93
Eugene Sokoletsky <eugene@eugenes-mbp.mynetworksettings.com>
2023-05-30 15:31:39 -0400

    PDF working

--- metro-electric/src/components/pdf/Terms.js
+++ metro-electric/src/components/pdf/Terms.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import MainContainer from '../MainContainer';
+import React from "react";
+import MainContainer from "../MainContainer";
 import { NavLink, Link as RouterLink } from "react-router-dom";
 import { Typography, Box } from "@mui/material";
 import MetroTekLogo from "../../images/logos/MetroTekLogo.jpg";
-
+import TermsWork from "../../files/TermsWork.pdf";
+import TermsEquipSales from "../../files/TermsEquipSales.pdf";
 
 function Terms() {
   const styles = {
@@ -15,58 +16,45 @@ function Terms() {
   };
 
   const onButtonDownloadWork = () => {
-    fetch('TermsWork.pdf').then(res => {
-      res.blob().then(blob => {
+    fetch(TermsWork).then((res) => {
+      res.blob().then((blob) => {
         const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement('a');
+        let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = 'TermsWork.pdf'
-        alink.click()
-      })
-    })
-  }
+        alink.download = "TermsWork.pdf";
+        alink.click();
+      });
+    });
+  };
 
   const onButtonDownloadSales = () => {
-    fetch('TermsEquipSales.pdf').then(res => {
-      res.blob().then(blob => {
+    fetch(TermsEquipSales).then((res) => {
+      res.blob().then((blob) => {
         const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement('a');
+        let alink = document.createElement("a");
         alink.href = fileURL;
-        alink.download = 'TermsEquipSales.pdf'
-        alink.click()
-      })
-    })
-  }
+        alink.download = "TermsEquipSales.pdf";
+        alink.click();
+      });
+    });
+  };
   return (
     <MainContainer>
-        <Box display='flex' justifyContent='center' marginTop='20px'>
-          <NavLink to='/'>
-            <img src={MetroTekLogo} alt='Footer Logo' style={styles.footerLogo} />
-          </NavLink>
-        </Box>
-        <Box
-          mt='50px'
-        >
-        <Typography
-          fontSize='30px'
-          fontWeight='900'
-        >
+      <Box display='flex' justifyContent='center' marginTop='20px'>
+        <NavLink to='/'>
+          <img src={MetroTekLogo} alt='Footer Logo' style={styles.footerLogo} />
+        </NavLink>
+      </Box>
+      <Box mt='50px'>
+        <Typography fontSize='30px' fontWeight='900'>
           Download Terms and Services!!!!!:
         </Typography>
-        </Box>
-        <Box
-        display='flex'
-        alignItems='center'
-        flexDirection='column'
-        mt='20px'
-      >
-
-        <Box
-          mb='20px'
-        >
-        <button onClick={onButtonDownloadWork}>
+      </Box>
+      <Box display='flex' alignItems='center' flexDirection='column' mt='20px'>
+        <Box mb='20px'>
+          <button onClick={onButtonDownloadWork}>
             Standard Terms and Conditions for Work
-        </button>
+          </button>
         </Box>
         <Box>
           <button onClick={onButtonDownloadSales}>
@@ -75,7 +63,7 @@ function Terms() {
         </Box>
       </Box>
     </MainContainer>
-  )
+  );
 }
 
-export default Terms
+export default Terms;
