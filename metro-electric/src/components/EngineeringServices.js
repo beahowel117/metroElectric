1ed614595395ffffaceb63b3ad0988f1f6456b89
Beth Howell <beahowel117@gmail.com>
2024-09-27 10:52:17 -0400

    added engineering services page

---
+++ metro-electric/src/components/EngineeringServices.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import cover from '../images/Services/Gear.jpg';
+import main from '../images/Services/PMH.jpg';
+
+
+
+function EngineeringServices() {
+  return (
+    <div>EngineeringServices</div>
+  )
+}
+
+export default EngineeringServices;
